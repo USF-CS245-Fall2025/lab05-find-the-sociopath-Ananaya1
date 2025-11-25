3efee8b8d99b66e04edb7ca3738ed8c8f3f41067
Ananaya Sood <ananyasood9@gmail.com>
2025-11-24 23:27:06 -0800

    Implement findTheSociopath method

--- sociopath/app/src/main/java/Sociopath.java
+++ sociopath/app/src/main/java/Sociopath.java
@@ -1,9 +1,40 @@
 import java.util.List;
 
-
 public class Sociopath {
 
-	public int findTheSociopath (int groupSize, List<int []> likeList) {
-		return 0;
-	}
+
+    public int findTheSociopath(int groupSize, List<int[]> likeList) {
+
+  
+        if (groupSize <= 1) {
+            return -1;
+        }
+
+  
+        int[] likesCount = new int[groupSize + 1];   
+        boolean[] likesSomeone = new boolean[groupSize + 1]; 
+
+        for (int[] pair : likeList) {
+            if (pair.length != 2) return -1;
+
+            int liker = pair[0];
+            int liked = pair[1];
+
+            if (liker < 1 || liker > groupSize || liked < 1 || liked > groupSize) {
+                return -1; 
+            }
+
+            likesSomeone[liker] = true;
+            likesCount[liked]++;
+        }
+
+
+        for (int person = 1; person <= groupSize; person++) {
+            if (!likesSomeone[person] && likesCount[person] == groupSize - 1) {
+                return person;
+            }
+        }
+
+        return -1; 
+    }
 }
